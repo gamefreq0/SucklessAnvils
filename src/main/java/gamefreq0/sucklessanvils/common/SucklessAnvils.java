f1edeb056f6259438320972494bdfc48d074d3b9
gamefreq0 <gamefreq0@gmail.com>
2024-04-24 12:07:48 -0400

    Add enough stuff to get mod to build, run, display info

---
+++ src/main/java/gamefreq0/sucklessanvils/common/SucklessAnvils.java
@@ -0,0 +1,7 @@
+package gamefreq0.sucklessanvils.common;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SucklessAnvils.MOD_ID)
+public class SucklessAnvils {
+    public static final String MOD_ID = "sucklessanvils";
+}
