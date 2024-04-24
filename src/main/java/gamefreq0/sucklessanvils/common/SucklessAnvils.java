c65414a3c7094083e612e45f99df655774ef8cca
gamefreq0 <gamefreq0@gmail.com>
2024-04-24 13:25:54 -0400

    GitHub shenanigans

--- src/main/java/gamefreq0/sucklessanvils/common/SucklessAnvils.java
+++ src/main/java/gamefreq0/sucklessanvils/common/SucklessAnvils.java
@@ -1,5 +1,6 @@
 package gamefreq0.sucklessanvils.common;
 import net.minecraftforge.fml.common.Mod;
+/*GitHub struggles, so here's a useless comment commit */
 
 @Mod(SucklessAnvils.MOD_ID)
 public class SucklessAnvils {
